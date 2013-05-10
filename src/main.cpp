4f121728efa40503845a8e7df2242442b1e6b87a
George Wu <georgeqwu@gmail.com>
2013-05-09 22:24:17 -0700

    wind and keys
    yay

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ using namespace Eigen;
 GLenum shadeModel = GL_SMOOTH;
 
 // for triangle wireframe
-GLenum polygonMode = GL_LINE;
+GLenum polygonMode = GL_FILL;
 
 // trying out timer
 
@@ -55,7 +55,7 @@ GLenum polygonMode = GL_LINE;
 Vector3f gravity(0.0f, -0.98f, 0.0f);
 
 //number of particles grid
-const int gridSize = 13;
+const int gridSize = 15;
 
 //Spring properties
 int numS;
@@ -71,6 +71,7 @@ Particles * p1=NULL;
 Particles * p2=NULL;
 Particles * currentP=NULL;
 Particles * nextP=NULL;
+Vector3f * windForces=NULL;
 
 // What do we want to draw?
 bool drawBalls = false, drawSprings = false;
@@ -85,7 +86,10 @@ GLuint floorTexture;
 // how tesselated is each patch?
 int patchTesselation = 5;
 
-float zoom = 8;
+float zoom = 12;
+
+//wind direction, changed by w a s d keys
+int windDirection = 0;
 
 
 
@@ -160,13 +164,13 @@ void initScene(){
   //end this section
 
 
-  /***
+  
 
   //this section from Paul's, edited though
   
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  //glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   
-  glViewport(0, 0, 640, 480);
+  //glViewport(0, 0, 640, 480);
   
   //set up projection matrix
   glMatrixMode(GL_PROJECTION);
@@ -206,7 +210,7 @@ void initScene(){
 
   //glOrtho(-4, 4, -4, 4, 1, -1); 
   
-  ***/
+  
   
 }
 
@@ -282,15 +286,18 @@ void ResetCloth()
 			p1[i*gridSize+j].fixed=false;
 		}
 	}
-
+	
 	//Fix the top left & top right balls in place
-	p1[0].fixed=true;
-	p1[gridSize-1].fixed=true;
+	p1[63].fixed=true;                            //0
+	p1[72].fixed=true;                   //gridSize-1
 	
 	//Fix the bottom left & bottom right balls
-	p1[gridSize*(gridSize-1)].fixed=true;
-	p1[gridSize*gridSize-1].fixed=true;
-
+	p1[153].fixed=true;        //gridSize*(gridSize-1)
+	p1[162].fixed=true;          //gridSize*gridSize-1
+	/***
+	p1[0].fixed = true;
+	p1[gridSize-1].fixed = true;
+	***/
 	//Copy the balls into the other array
 	for(int i=0; i<numP; ++i)
 		p2[i]=p1[i];
@@ -402,8 +409,8 @@ void ResetCloth()
 
 //perform per frame updates
 void UpdateFrame() {
-	currentP[0].fixed = false;
-	currentP[gridSize-1].fixed = false;
+	currentP[0].fixed = true;
+	currentP[gridSize-1].fixed = true;
 	currentP[gridSize*(gridSize-1)].fixed = false;
 	currentP[gridSize*gridSize-1].fixed = false;
 	
@@ -421,7 +428,64 @@ void UpdateFrame() {
 
 		springs[i].tension=springs[i].springConst*extension/springs[i].restLength;
 	}
+	
+	Vector3f windDirVect(0,0,0);
+	switch (windDirection) {
+		case 0:
+			windDirVect << 0,0,0;
+			break;
+		case 1:
+			windDirVect << 0,0,-.3;
+			break;
+		case 2:
+			windDirVect << 0,0,.3;
+			break;
+		case 3:
+			windDirVect << -.3,0,0;
+			break;
+		case 4:
+			windDirVect << .3,0,0;
+			break;
+	}
 
+	//need an array to store all the force values for each particle
+	windForces=new Vector3f[numP];
+	for(int k=0; k<numP; ++k) {
+		windForces[k] << 0,0,0;
+	}
+	Vector3f tri1_1, tri1_2, tri1_3, tri2_1, tri2_2, tri2_3, 
+		tri1_norm, tri2_norm, tri1_force, tri2_force;
+	//probably do wind calculation here, and then add in later
+	for(int i=0; i<gridSize-1; ++i) {
+		for(int j=0; j<gridSize-1; ++j) {
+			// do calc for two triangles
+			// first triangle
+			tri1_1 = currentP[i*gridSize+j].position;
+			tri1_2 = currentP[i*gridSize+j+1].position;
+			tri1_3 = currentP[(i+1)*gridSize+j].position;
+			// second triangle
+			tri2_1 = currentP[(i+1)*gridSize+j].position;
+			tri2_2 = currentP[i*gridSize+j+1].position;
+			tri2_3 = currentP[(i+1)*gridSize+j+1].position;
+			// find normals for triangles
+			tri1_norm = (tri1_2 - tri1_1).cross(tri1_3 - tri1_1);
+			tri2_norm = (tri2_2 - tri2_1).cross(tri2_3 - tri2_1);
+			tri1_norm.normalize();
+			tri2_norm.normalize();
+			// forces whee
+			tri1_force = tri1_norm * tri1_norm.dot(windDirVect);
+			tri2_force = tri2_norm * tri2_norm.dot(windDirVect);
+			windForces[i*gridSize+j] += tri1_force;
+			windForces[i*gridSize+j+1] += tri1_force;
+			windForces[(i+1)*gridSize+j] += tri1_force;
+			windForces[(i+1)*gridSize+j] += tri2_force;
+			windForces[i*gridSize+j+1] += tri2_force;
+			windForces[(i+1)*gridSize+j+1] += tri2_force;
+		}
+	}
+	
+	
+	
 	//Calculate the nextP from the currentP
 	for(int i=0; i<numP; ++i)
 	{
@@ -434,12 +498,13 @@ void UpdateFrame() {
 		if(currentP[i].fixed)
 		{
 			nextP[i].position=currentP[i].position;
-			nextP[i].velocity<<(0,0,0);
+			Vector3f zeroVect(0,0,0);
+			nextP[i].velocity = zeroVect;
 		}
 		else
 		{
 			//Calculate the force on this ball
-			Vector3f force=gravity;
+			Vector3f force=gravity + windForces[i];
 
 			//Loop through springs
 			for(int j=0; j<numS; ++j)
@@ -464,6 +529,8 @@ void UpdateFrame() {
 					force+=springs[j].tension*tensionDirection;
 				}
 			}
+			
+			
 
 			//Calculate the acceleration
 			Vector3f acceleration=force/currentP[i].mass;
@@ -490,7 +557,7 @@ void UpdateFrame() {
 			}
 		}
 	}
-
+	//cout << "here2" << endl;
 	//Swap the currentP and newBalls pointers
 	Particles * temp=currentP;
 	currentP=nextP;
@@ -534,7 +601,7 @@ void UpdateFrame() {
 			n3+=normal;
 		}
 	}
-
+	//cout << "here3" << endl;
 	//Normalize normals
 	for(int i=0; i<numP; ++i)
 		currentP[i].normal.normalize();
@@ -551,20 +618,40 @@ void RenderFrame()
 	//Clear buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();										//reset modelview matrix
+	glLoadIdentity();		
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);								//reset modelview matrix
 	
-	//glutWireTeapot(1.0);
-	
-	glEnable(GL_DEPTH_TEST);
 	
 	glOrtho(-1.0*zoom, 1.0*zoom, -1.0*zoom, 1.0*zoom, -4.0, 4.0);
 	
-	glShadeModel(shadeModel);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);	
+	//glShadeModel(shadeModel);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);	
 
-	glColor3f (0.3, 0.8, 1.0);
-	//glTranslatef(0.0f, 0.0f, -28.0f);
+
+	glTranslatef(0,0,-1.5);
+
+	/***
+	//sphere
+
+	glEnable(GL_LIGHTING);
+	GLfloat mat1 [] = {1.0f, 0.0f, 0.0f, 0.0f};
+	GLfloat mat2 [] = {1.0f, 0.0f, 0.0f, 0.0f};
+	GLfloat mat3 [] = {1.0f, 1.0f, 1.0f, 1.0f};
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat1);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat2);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat3);
+	glMaterialf(GL_FRONT, GL_SHININESS, 32.0f);
+	glEnable(GL_CULL_FACE);
+	glTranslatef(0,0,-1.5);
 	glutSolidSphere(sphereRadius, 48, 24);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_CULL_FACE);
+	GLfloat mat4 [] = {0.0f, 0.0f, 0.0f, 0.0f};
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat4);
+	
+	***/
+	
+	
 	
 	/*** 
 	//triangle works....
@@ -601,22 +688,22 @@ void RenderFrame()
 	
 	***/
 	
-	glColor3f (1.0, 0.5, 0.3);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
 	//Draw cloth as triangles
 	if(drawTriangles)
 	{
-		/***
+		
 		glEnable(GL_LIGHTING);
-		GLfloat tri_mat1 [] = {0.8f, 0.0f, 1.0f};
-		GLfloat tri_mat2 [] = {0.8f, 0.0f, 1.0f};
-		GLfloat tri_mat3 [] = {1.0f, 1.0f, 0.0f};
-		GLfloat tri_mat4 [] = {1.0f, 1.0f, 0.0f};
+		GLfloat tri_mat3 [] = {1.0f, 0.5f, 0.31f};
+		GLfloat tri_mat4 [] = {1.0f, 0.5f, 0.31f};
+		GLfloat tri_mat1 [] = {1.0f, 1.0f, 0.0f};
+		GLfloat tri_mat2 [] = {1.0f, 1.0f, 0.0f};
 		glMaterialfv(GL_FRONT, GL_AMBIENT, tri_mat1);	//set material
 		glMaterialfv(GL_FRONT, GL_DIFFUSE, tri_mat2);
 		glMaterialfv(GL_BACK, GL_AMBIENT, tri_mat3);
 		glMaterialfv(GL_BACK, GL_DIFFUSE, tri_mat4);
-		***/
+		
+		
 		
 		//glBegin(GL_TRIANGLES);
 		{
@@ -627,65 +714,53 @@ void RenderFrame()
 					//cout << "drawing triangles!" << endl;
 					//cout << currentP[i*gridSize+j].position << endl << endl;
 					glBegin(GL_POLYGON);
-					/***
 					glNormal3f(currentP[i*gridSize+j].normal(0),
 						currentP[i*gridSize+j].normal(1),
 						currentP[i*gridSize+j].normal(2));
-					***/
 					glVertex3f(currentP[i*gridSize+j].position(0),
 						currentP[i*gridSize+j].position(1),
-						0);
-					/***
+						currentP[i*gridSize+j].position(2));
 					glNormal3f(currentP[i*gridSize+j+1].normal(0),
 						currentP[i*gridSize+j+1].normal(1),
 						currentP[i*gridSize+j+1].normal(2));
-					***/
 					glVertex3f(currentP[i*gridSize+j+1].position(0),
 						currentP[i*gridSize+j+1].position(1),
-						0);
-					/***	
+						currentP[i*gridSize+j+1].position(2));
 					glNormal3f(currentP[(i+1)*gridSize+j].normal(0),
 						currentP[(i+1)*gridSize+j].normal(1),
 						currentP[(i+1)*gridSize+j].normal(2));
-					***/	
 					glVertex3f(currentP[(i+1)*gridSize+j].position(0),
 						currentP[(i+1)*gridSize+j].position(1),
-						0);
+						currentP[(i+1)*gridSize+j].position(2));
 						
 					glEnd();
 					
 					glBegin(GL_POLYGON);
-					/***
 					glNormal3f(currentP[(i+1)*gridSize+j].normal(0),
 						currentP[(i+1)*gridSize+j].normal(1),
 						currentP[(i+1)*gridSize+j].normal(2));
-					***/	
 					glVertex3f(currentP[(i+1)*gridSize+j].position(0),
 						currentP[(i+1)*gridSize+j].position(1),
-						0);
-					/***	
+						currentP[(i+1)*gridSize+j].position(2));
 					glNormal3f(currentP[i*gridSize+j+1].normal(0),
 						currentP[i*gridSize+j+1].normal(1),
 						currentP[i*gridSize+j+1].normal(2));
-					***/	
 					glVertex3f(currentP[i*gridSize+j+1].position(0),
 						currentP[i*gridSize+j+1].position(1),
-						0);
-					/***	
+						currentP[i*gridSize+j+1].position(2));
 					glNormal3f(currentP[(i+1)*gridSize+j+1].normal(0),
 						currentP[(i+1)*gridSize+j+1].normal(1),
 						currentP[(i+1)*gridSize+j+1].normal(2));
-					***/	
 					glVertex3f(currentP[(i+1)*gridSize+j+1].position(0),
 						currentP[(i+1)*gridSize+j+1].position(1),
-						0);
+						currentP[(i+1)*gridSize+j+1].position(2));
 						
 					glEnd();
 				}
 			}
 		}
 		//glEnd();
-		//glDisable(GL_LIGHTING);
+		glDisable(GL_LIGHTING);
 	}
 	
 	
@@ -713,99 +788,6 @@ void DemoShutdown() {
 
 }
 
-//***************************************************
-// function that does the actual drawing
-//***************************************************
-void myDisplay() {
-
-
-  //----------------------- ----------------------- -----------------------
-  // This is a quick hack to add a little bit of animation.
-  static float tip = 0.5f;
-  const  float stp = 0.0001f;
-  const  float beg = 0.1f;
-  const  float end = 0.9f;
-
-  tip += stp;
-  if (tip>end) tip = beg;
-  //----------------------- ----------------------- -----------------------
-
-
-  glClear(GL_COLOR_BUFFER_BIT);                // clear the color buffer (sets everything to black)
-
-  glMatrixMode(GL_MODELVIEW);                  // indicate we are specifying camera transformations
-  glLoadIdentity();                            // make sure transformation is "zero'd"
-
-  //----------------------- code to draw objects --------------------------
-  // Rectangle Code
-  //glColor3f(red component, green component, blue component);
-  glColor3f(1.0f,0.0f,0.0f);                   // setting the color to pure red 90% for the rect
-
-  glBegin(GL_POLYGON);                         // draw rectangle 
-  //glVertex3f(x val, y val, z val (won't change the point because of the projection type));
-  glVertex3f(-0.8f, 0.0f, 0.0f);               // bottom left corner of rectangle
-  glVertex3f(-0.8f, 0.5f, 0.0f);               // top left corner of rectangle
-  glVertex3f( 0.0f, 0.5f, 0.0f);               // top right corner of rectangle
-  glVertex3f( 0.0f, 0.0f, 0.0f);               // bottom right corner of rectangle
-  glEnd();
-  
-  glColor3f(0.5f,0.0f,1.0f); 
-  
-  glBegin(GL_POLYGON);                         // draw left eye
-  glVertex3f(-0.60f, -0.3f, 0.0f);               // bottom left corner of rectangle
-  glVertex3f(-0.60f, -0.1f, 0.0f);               // top left corner of rectangle
-  glVertex3f(-0.55f, -0.1f, 0.0f);               // top right corner of rectangle
-  glVertex3f(-0.55f, -0.3f, 0.0f);               // bottom right corner of rectangle
-  glEnd();
-  
-  glBegin(GL_POLYGON);                         // draw left eye
-  glVertex3f(-0.45f, -0.3f, 0.0f);               // bottom left corner of rectangle
-  glVertex3f(-0.45f, -0.1f, 0.0f);               // top left corner of rectangle
-  glVertex3f(-0.40f, -0.1f, 0.0f);               // top right corner of rectangle
-  glVertex3f(-0.40f, -0.3f, 0.0f);               // bottom right corner of rectangle
-  glEnd();
-  
-  glBegin(GL_POLYGON); 						
-  glVertex3f(-0.8f, -0.38f, 0.0f);
-  glVertex3f(-0.2f, -0.38f, 0.0f);
-  glVertex3f(-0.38f, -.53f, 0.0f);
-  glVertex3f(-0.5f, -0.60f, 0.0f);
-  glVertex3f(-0.62f, -.53f, 0.0f);
-  glEnd();
-  
-  glColor3f(.36f,.25f,.20f); 
-  
-  glBegin(GL_POLYGON);						//draw cross
-  glVertex3f(.25f, .78f, 0.0f);
-  glVertex3f(.75f, .78f, 0.0f);
-  glVertex3f(.75f, .72f, 0.0f);
-  glVertex3f(.25f, .72f, 0.0f);
-  glEnd();
-  
-  glBegin(GL_POLYGON);
-  glVertex3f(.47f, .95f, 0.0f);
-  glVertex3f(.53f, .95f, 0.0f);
-  glVertex3f(.53f, .10f, 0.0f);
-  glVertex3f(.47f, .10f, 0.0f);
-  glEnd();
-  
-  
-  // Triangle Code
-  glColor3f(1.0f,0.5f,0.0f);                   // setting the color to orange for the triangle
-
-  float basey = -sqrt(0.48f);                  // height of triangle = sqrt(.8^2-.4^2)
-  glBegin(GL_POLYGON);
-  glVertex3f(tip,  0.0f, 0.0f);                // top tip of triangle
-  glVertex3f(0.1f, basey, 0.0f);               // lower left corner of triangle
-  glVertex3f(0.9f, basey, 0.0f);               // lower right corner of triangle
-  glEnd();
-  //-----------------------------------------------------------------------
-
-  glFlush();
-  glutSwapBuffers();                           // swap buffers (we earlier set double buffer)
-  
-  
-}
 
 
 //****************************************************
@@ -817,6 +799,40 @@ void myFrameMove() {
   Sleep(10);                                   //give ~10ms back to OS (so as not to waste the CPU)
 #endif
   glutPostRedisplay(); // forces glut to call the display function (myDisplay())
+}
+
+//****************************************************
+// toggle commands
+//****************************************************
+void mykeyFunc(unsigned char key, int x, int y){
+	switch (key) {
+		case 'q' :
+			windDirection = 0;
+			break;
+		case 'w' :
+			windDirection = 1;
+			break;
+		case 's' :
+			windDirection = 2;
+			break;
+		case 'a' :
+			windDirection = 3;
+			break;
+		case 'd' :
+			windDirection = 4;
+			break;
+		case 'p' :
+			if (polygonMode == GL_LINE) {
+				polygonMode = GL_FILL;
+			} else {
+				polygonMode = GL_LINE;
+			}
+			break;
+	}
+  
+  
+  
+  
 }
 
 
@@ -860,12 +876,14 @@ int main(int argc, char *argv[]) {
   glutCreateWindow("CS184!");
 
   initScene();                                 // quick function to set up scene
-  
+  //cout << "initScene" << endl;
 
   DemoInit();
-
+  //cout << "demoInit" << endl;
   glutDisplayFunc(UpdateFrame);                  // function to run when its time to draw something
+  
   glutReshapeFunc(myReshape);                  // function to run when the window gets resized
+  glutKeyboardFunc(mykeyFunc);
   glutIdleFunc(myFrameMove);                   // function to run when not handling any other task
   glutMainLoop();                              // infinite loop that will keep drawing and resizing and whatever else
   
